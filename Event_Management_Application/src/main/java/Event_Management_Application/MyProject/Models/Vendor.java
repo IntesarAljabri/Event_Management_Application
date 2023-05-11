0022291b0c9d89ee187449d11e30619f76e8dd04
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-11 11:13:47 +0400

    DONEMODELS

--- Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Models/Vendor.java
+++ Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Models/Vendor.java
@@ -1,9 +1,11 @@
 package Event_Management_Application.MyProject.Models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,4 +13,30 @@ import javax.persistence.*;
 @Entity
 @Table(name = "vendor")
 public class Vendor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    Boolean manufacturers_type;
+    Boolean wholesalers_type;
+    Boolean retailers_type;
+    Boolean maintenance_type;
+    Boolean service_type;
+
+    @ManyToMany(mappedBy = "vendors")
+    @JsonIgnore
+    private List<Event> events;
+
 }
--- Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Models/Venue.java
+++ Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Models/Venue.java
@@ -1,9 +1,11 @@
 package Event_Management_Application.MyProject.Models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,4 +13,21 @@ import javax.persistence.*;
 @Entity
 @Table(name = "venue")
 public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String address;
+    String capacity;
+    String email;
+    Integer phone;
+
+    @OneToMany(mappedBy = "venue")
+    @JsonIgnore
+    private List<Event> events;
+
+    @OneToMany(mappedBy = "venue")
+    @JsonIgnore
+    private List<Organizer> organizers;
 }
