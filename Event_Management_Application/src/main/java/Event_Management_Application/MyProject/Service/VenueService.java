a4317724a1e43944a3259436b3f6bc97d65372be
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-11 11:42:58 +0400

    DONE

--- Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Service/VenueService.java
+++ Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Service/VenueService.java
@@ -1,8 +1,25 @@
 package Event_Management_Application.MyProject.Service;
 
+import Event_Management_Application.MyProject.Models.Vendor;
+import Event_Management_Application.MyProject.Models.Venue;
+import Event_Management_Application.MyProject.Repositories.VendorRepository;
+import Event_Management_Application.MyProject.Repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VenueService {
+    @Autowired
+    VenueRepository venueRepository;
+    public List<Venue> getAllVenue() {
 
+        return venueRepository.findAll();
+    }
+
+    public Venue getVenueById(Long id) {
+
+        return venueRepository.findById(id).get();
+    }
 }
