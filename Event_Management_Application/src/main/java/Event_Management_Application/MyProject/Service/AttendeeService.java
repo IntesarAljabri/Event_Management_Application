ec38c767f24d926e90109a61274358f2bddc08fc
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-11 11:23:59 +0400

    Service

--- Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Service/AttendeeService.java
+++ Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Service/AttendeeService.java
@@ -1,7 +1,23 @@
 package Event_Management_Application.MyProject.Service;
 
+import Event_Management_Application.MyProject.Models.Attendee;
+import Event_Management_Application.MyProject.Repositories.AttendeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AttendeeService {
+    @Autowired
+    AttendeeRepository attendeeRepository;
+    public List<Attendee> getAllAttendees() {
+
+        return attendeeRepository.findAll();
+    }
+
+    public Attendee getAttendeeById(Long id) {
+
+        return attendeeRepository.findById(id).get();
+    }
 }
--- Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Service/OrganizerService.java
+++ Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Service/OrganizerService.java
@@ -1,7 +1,25 @@
 package Event_Management_Application.MyProject.Service;
 
+import Event_Management_Application.MyProject.Models.Event;
+import Event_Management_Application.MyProject.Models.Organizer;
+import Event_Management_Application.MyProject.Repositories.EventRepository;
+import Event_Management_Application.MyProject.Repositories.OrganizerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrganizerService {
+    @Autowired
+    OrganizerRepository organizerRepository;
+    public List<Organizer> getAllOrganizer() {
+
+        return organizerRepository.findAll();
+    }
+
+    public Organizer getOrganizerById(Long id) {
+
+        return organizerRepository.findById(id).get();
+    }
 }
