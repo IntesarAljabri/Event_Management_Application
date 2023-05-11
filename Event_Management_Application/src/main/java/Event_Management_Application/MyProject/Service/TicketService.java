a874656a2081c0e275db89a11539cb3e9263997d
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-11 11:41:21 +0400

    Service

--- Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Service/TicketService.java
+++ Event_Management_Application/src/main/java/Event_Management_Application/MyProject/Service/TicketService.java
@@ -1,7 +1,26 @@
 package Event_Management_Application.MyProject.Service;
 
+import Event_Management_Application.MyProject.Models.Sponsor;
+import Event_Management_Application.MyProject.Models.Ticket;
+import Event_Management_Application.MyProject.Repositories.SponsorRepository;
+import Event_Management_Application.MyProject.Repositories.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TicketService {
+
+    @Autowired
+    TicketRepository ticketRepository;
+    public List<Ticket> getAllTicket() {
+
+        return ticketRepository.findAll();
+    }
+
+    public Ticket getTicketById(Long id) {
+
+        return ticketRepository.findById(id).get();
+    }
 }
