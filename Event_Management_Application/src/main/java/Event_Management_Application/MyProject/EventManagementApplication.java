6faf84332a5eb05a796bfce9a6632ef537ce4b16
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-11 09:13:08 +0400

    NEW

---
+++ Event_Management_Application/src/main/java/Event_Management_Application/MyProject/EventManagementApplication.java
@@ -0,0 +1,13 @@
+package Event_Management_Application.MyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementApplication.class, args);
+	}
+
+}
---
+++ Event_Management_Application/src/test/java/Event_Management_Application/MyProject/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Event_Management_Application.MyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
